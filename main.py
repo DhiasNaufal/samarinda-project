86f3d265ce51f003291e28f471cfd073d8056f8a
habibmuhammadthariq <thariqhabibmuhammad@gmail.com>
2025-04-09 18:52:05 +0700

    feat: add splash screen

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import sys
-from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication, QSplashScreen
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt, QTimer
 from ui.main_window import MainWindow
+
+def show_main_window():
+    window.show()
+    splash.finish(window)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    
+    # Splash Screen
+    splash_pixmap = QPixmap("assets/img/ugm.png")
+    splash = QSplashScreen(splash_pixmap, Qt.WindowType.WindowStaysOnTopHint)
+    splash.setMask(splash_pixmap.mask())
+    splash.setWindowOpacity(0.9)
+    splash.show()
+    
     window = MainWindow()
-    window.show()
+
+    # Delay showing the main window by 2 seconds (non-blocking)
+    QTimer.singleShot(2000, show_main_window)
+    
     sys.exit(app.exec())
